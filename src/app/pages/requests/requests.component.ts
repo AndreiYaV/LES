3a75e13c21001643bfe9f4a3f86d08fa9829d237
AndreiYaV <andrei.yakimenko@gmail.com>
2022-01-17 17:28:06 +0300

    2022_01_17

--- src/app/pages/requests/requests.component.ts
+++ src/app/pages/requests/requests.component.ts
@@ -16,6 +16,8 @@ import {switchMap, tap} from "rxjs/operators";
 export class RequestsComponent implements OnInit {
   requestTypes: IRequestType[] = [];
   requests: Requests.IRequest[] = [];
+  actualRequests: Requests.IRequest[] = [];
+  passedRequests: Requests.IRequest[] = [];
 
   constructor(
     private route: ActivatedRoute,
@@ -30,7 +32,18 @@ export class RequestsComponent implements OnInit {
     this.requestService.getRequests()
       .subscribe(req => {
         (this.requests as IRequest[] | undefined) = req.requests
+        this.sortRequests(this.requests)
       })
+  }
+
+  private sortRequests(requests: IRequest[]): IRequest[] {
+    return requests.filter(request => {
+      if (new Date(request.end_date).getTime() > Date.now()) {
+        this.actualRequests.push(request)
+      } else {
+        this.passedRequests.push(request)
+      }
+    });
   }
 
   sendRequest(req: IRequest) {
@@ -45,7 +58,9 @@ export class RequestsComponent implements OnInit {
       switchMap(user => this.requestService.createRequest(user).pipe(
         tap(response => {
           (this.requests as IRequest[] | undefined) = response.requests
-        })))
+        }))),
+      switchMap(user => this.userService.addUserRequest(user).pipe(
+      ))
     ).subscribe()
   }
 
